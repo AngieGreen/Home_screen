ecedacea820bb81722f9962bd728e8a77bdf7871
Angie Green <angie@greenorleans.com>
2015-02-25 10:48:15 -0600

    Add background image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     # Adjust message by time of day
     @greet_message = case time_now.hour
       when 5..11
-        "Good morning, beautiful!"
+        "Good morning, beautiful! <i class='fa fa-motorcycle'></i>".html_safe
       when 12..15
         "Happy afternoon, sunshine!"
       when 16..19
