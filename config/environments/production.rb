01c448ebc1d93c2ba22f8042204488932f03deb7
Angie Green <angie@greenorleans.com>
2015-03-09 09:10:56 -0500

    Generate clearance and migrated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # TODO update this info & uncomment when you push to Heroku
+  # set URL for the mailer
+  # config.action_mailer.default_url_options = { host: 'localhost:3000'}
+
 end
